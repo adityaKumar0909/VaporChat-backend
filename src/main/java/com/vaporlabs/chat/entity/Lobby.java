0cc7a09e67b8cc72114a2a4aa13e67b2e43b7e4f
adityaKumar0909 <kadi93030@gmail.com>
2026-04-07 15:11:04 +0530

    added entities for Redis setup

--- src/main/java/com/vaporlabs/chat/entity/Lobby.java
+++ src/main/java/com/vaporlabs/chat/entity/Lobby.java
@@ -1,4 +1,26 @@
 package com.vaporlabs.chat.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.TimeToLive;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@RedisHash(value = "Lobby")
 public class Lobby {
+    @Id
+    private UUID id;
+
+    private String joinCode;
+
+    private LocalDateTime createdAt;
+
+    @TimeToLive
+    private long ttl;
 }
--- src/main/java/com/vaporlabs/chat/entity/Message.java
+++ src/main/java/com/vaporlabs/chat/entity/Message.java
@@ -1,4 +1,22 @@
 package com.vaporlabs.chat.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
 public class Message {
+
+    private String id = UUID.randomUUID().toString();
+
+    private String lobbyId;
+
+    private String encryptedMessage;
+
+    private LocalDateTime timestamp = LocalDateTime.now();
+
 }
